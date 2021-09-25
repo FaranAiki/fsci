057b6d84a5bc868e86354108dafd0c0998ffeeb4
FaranAiki <abang.faran.aiki@gmail.com>
2021-09-25 17:07:23 +0700

    Updating with date of 2021-09-25

--- gitupload.sh
+++ gitupload.sh
@@ -5,5 +5,4 @@ git add .
 git commit -m "Updating with date of $(date +%F)"
 git remote add origin https://github.com/faranaiki/fsci.git
 git remote show origin
-git push --force -u origin main
-git pull origin main
+git push -u origin master
